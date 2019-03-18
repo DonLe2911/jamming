a81ac6b384cd24677bb66282422f7694ad46bcd4
donle2911 <donle@kainos.com>
2019-03-18 15:15:09 +0000

    fixed bug of + and - not rendering. All other funtionality seems to work!

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -6,11 +6,7 @@ class Track extends React.Component {
 constructor(props){
   super(props);
   this.addTrack = this.addTrack.bind(this);
-}
-
-
-addTrack(){
-  this.props.onAdd(this.props.track);
+  this.removeTrack = this.removeTrack.bind(this);
 }
 
 renderAction() {
@@ -21,13 +17,22 @@ renderAction() {
  }
 }
 
-    render() {
+addTrack(){
+  this.props.onAdd(this.props.track);
+}
+
+removeTrack() {
+    this.props.onRemove(this.props.track);
+  }
+
+render() {
      return (
        <div className="Track">
          <div className="Track-information">
            <h3>{this.props.track.name}</h3>
            <p>{this.props.track.artist} | {this.props.track.album}</p>
          </div>
+          {this.renderAction()}
        </div>
      )
    }
